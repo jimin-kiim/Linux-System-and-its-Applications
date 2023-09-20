fb2028a0b9d740eae29ef6e43bae2303ecd82685
jimin-kiim <0305jimin@gmail.com>
2023-09-20 10:28:17 +0000

    feat: validating inputs

--- multi-table.sh
+++ multi-table.sh
@@ -1,4 +1,16 @@
 #!/bin/sh
+if [ $# -ne 2 ]
+then 
+  echo "Invalid input"
+  exit 0
+fi
+
+if [ $1 -le 0 -o $2 -le 0 ]
+then 
+  echo "Input must be greater than 0"
+  exit 0
+fi
+
 for i in $(seq 1 $1) 
 do 
  for j in $(seq 1 $2)
