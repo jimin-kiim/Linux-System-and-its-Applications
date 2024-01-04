5b49d1c529d5dac94212b613397b02c4dbc1fb81
jimin-kiim <0305jimin@naver.com>
2024-01-05 05:09:34 +0900

    Refactor: deleting codes of including redundant header files

--- assignment10/spinlock_module/spinlock_module.c
+++ assignment10/spinlock_module/spinlock_module.c
@@ -1,17 +1,12 @@
 // #--------- spinlock_module.c ---------#
-#include <linux/kernel.h>
-#include <linux/module.h>
 #include <linux/init.h>
 #include <linux/kthread.h>
-#include <linux/atomic.h>
 #include <linux/delay.h>
-#include <linux/spinlock.h>
-#include <linux/slab.h>
-#include <linux/sched.h>
 #include "../calclock.h"
 #include "../linked_list_impl.h"
 
 #define NUM_THREADS 4
+
 static struct task_struct *threads[NUM_THREADS];
 
 extern unsigned long long count_insert, count_search, count_delete;
@@ -22,11 +17,11 @@ void set_iter_range(int thread_id, int range_bound[2]) {
 	range_bound[1] = range_bound[0] + 249999;
 }
 
-static int work_fn(void *data)
-{
+static int work_fn(void *data) {
 	void *ret;
 	int range_bound[2];
 	int thread_id = *(int*) data;
+	
 	set_iter_range(thread_id, range_bound);
 	ret = add_to_list(thread_id, range_bound);
 	search_list(thread_id, ret, range_bound);
@@ -50,11 +45,13 @@ int __init spinlock_module_init(void) {
 		*arg = i;
 		threads[i] = kthread_run(work_fn, (void*)arg, "thread%d", i);
 	}
+
 	return 0;
 }
 
 void __exit spinlock_module_cleanup(void) {
 	int i;
+
 	printk(KERN_INFO"%s: Spinlock linked list insert time: %llu ms, count: %llu", __func__, time_insert, count_insert);
 	printk(KERN_INFO"%s: Spinlock linked list search time: %llu ms, count: %llu", __func__, time_search, count_search);
 	printk(KERN_INFO"%s: Spinlock linked list delete time: %llu ms, count: %llu", __func__, time_delete, count_delete);
